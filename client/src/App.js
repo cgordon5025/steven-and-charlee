6bffd27716288faaacd0d0fbd651f70994a1d89f
Charlee Gordon <cgordon5025@gmail.com>
2022-12-23 15:37:34 -0500

    took out auth code for now

--- client/src/App.js
+++ client/src/App.js
@@ -4,27 +4,29 @@ import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@ap
 import { Routes, Route } from 'react-router-dom';
 //import pages 
 
-const authLink = setContext((_, { header }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-})
+// const authLink = setContext((_, { header }) => {
+//   const token = localStorage.getItem('id_token');
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// })
 
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  // link: authLink.concat(httpLink),
+  uri: '/graphql',
   cache: new InMemoryCache()
 });
 function App() {
   return (
     <ApolloProvider client={client}>
+      <h2> Hello</h2>
       <Routes>
         <Route />
       </Routes>
