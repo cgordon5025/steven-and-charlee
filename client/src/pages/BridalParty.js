fc9079f22a2f71f5f34ad10667c7d224b7860ae8
Charlee Gordon <cgordon5025@gmail.com>
2023-01-01 14:44:25 -0500

    added button to reroute

--- client/src/pages/BridalParty.js
+++ client/src/pages/BridalParty.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from 'react-bootstrap/Button';
-
+import { Link } from "react-router-dom";
 function BridalParty() {
     const [showModal, setShowModal] = useState(true)
     const [validPass, setValidPass] = useState()
@@ -22,6 +22,9 @@ function BridalParty() {
         passInput.password == "Password" ? (handleCorrect()) : (handleWrong());
     }
 
+    const reroute = () => {
+        setShowModal(false)
+    }
     const handleChange = (event) => {
         const { name, value } = event.target
         setPassInput({
@@ -29,18 +32,23 @@ function BridalParty() {
             [name]: value
         })
     }
+    const styles = {
+        btns: {
+            margin: "1%"
+        }
+    }
     return (
         <>
             <div className="supreme-container">
                 <p> Hello</p>
-                <p> Info for the birdal party</p>
+                <p> Info for the bridal party</p>
             </div>
 
             <Modal show={showModal} onHide={handleClose}>
                 <Modal.Header>
                     Please enter the password to gain access to this page
                 </Modal.Header>
-                <Modal.Body>
+                <Modal.Body style={{ display: "flex", flexDirection: "Column", alignItems: "center" }}>
                     <form>
                         <input
                             className="form-input"
@@ -52,17 +60,16 @@ function BridalParty() {
                         >
                         </input>
                     </form>
-                </Modal.Body>
-                <Modal.Footer style={{ display: "flex", flexDirection: "Column" }}>
                     {showResp ? (
-                        <p> You have entered the wrong password</p>
+                        <p style={{ marginTop: "3%" }}> You have entered the wrong password</p>
                     ) : (
                         <></>
                     )
                     }
-                    < Button onClick={handleClose}>Submit</Button>
-
-
+                </Modal.Body>
+                <Modal.Footer style={{ display: "flex", justifyContent: "space-evenly" }}>
+                    < Button style={styles.btns} onClick={handleClose}>Submit</Button>
+                    < Link style={styles.btns} className="btn btn-primary" onClick={reroute} to='/'> Not in the Wedding Party</Link>
                 </Modal.Footer>
             </Modal>
         </>
