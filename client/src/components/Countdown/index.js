ae4d29ae464809698e916c6b0025a66ee98130de
Charlee Gordon <cgordon5025@gmail.com>
2023-01-13 17:22:31 -0500

    countdown implemented

---
+++ client/src/components/Countdown/index.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+
+const Countdown = () => {
+
+    const [expiryTime, setExpiryTime] = useState("14 jan 2023 08:00:00");
+    const [countdownTime, setCountdownTime] = useState(
+        {
+            countdownDays: '',
+            countdownHours: '',
+            countdownMinutes: '',
+            countdownSeconds: ''
+        }
+    );
+
+    const countdownTimer = () => {
+        const timeInterval = setInterval(() => {
+            const countdownDateTime = new Date(expiryTime).getTime();
+            const currentTime = new Date().getTime();
+            const remainingDayTime = countdownDateTime - currentTime;
+            const totalDays = Math.floor(remainingDayTime / (1000 * 60 * 60 * 24));
+            const totalHours = Math.floor((remainingDayTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const totalMinutes = Math.floor((remainingDayTime % (1000 * 60 * 60)) / (1000 * 60));
+            const totalSeconds = Math.floor((remainingDayTime % (1000 * 60) / 1000));
+
+            const runningCountdownTime = {
+                countdownDays: totalDays,
+                countdownHours: totalHours,
+                countdownMinutes: totalMinutes,
+                countdownSeconds: totalSeconds
+            }
+            setCountdownTime(runningCountdownTime)
+
+            if (remainingDayTime < 0) {
+                clearInterval(timeInterval);
+                setExpiryTime(false)
+            }
+        }, 1000);
+    }
+
+    useEffect(() => {
+        countdownTimer()
+    });
+
+    return (
+        <>
+            {expiryTime !== false ? (
+                // <div style={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
+                <div>
+                    <h1 className="text-center"> Countdown to Our Wedding</h1>
+                    <div style={{ display: "flex", justifyContent: "center", justifyContent: "space-evenly" }}>
+                        <h2>{countdownTime.countdownDays}<span> : Days</span></h2>
+                        <h2>{countdownTime.countdownHours}<span> : Hours</span></h2>
+                        <h2>{countdownTime.countdownMinutes}<span> : Minutes</span></h2>
+                        <h2>{countdownTime.countdownSeconds}<span> : Seconds</span></h2>
+                    </div>
+                </div>
+            ) : (
+                <h2>We got married on *insert date here*</h2>
+            )}
+        </>
+    )
+}
+
+export default Countdown
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,17 @@
 import React from "react";
-import Countdown from "react-countdown";
+// import Countdown from "react-countdown";
 import DateCountdown from 'react-date-countdown-timer'
-
+import Countdown from "../components/Countdown";
 const Home = () => {
     return (
         <div className="container">
             <div id="about">
                 {/* this is is year, month, date order  */}
-                <Countdown date={'2024-01-14T16:00:00'} />
+                <div>
+                    <h2> Countdown to our Wedding</h2>
+                    <Countdown />
+                    {/* <Countdown date={'2024-01-14T16:00:00'} /> */}
+                </div>
                 {/* <DateCountdown dateTo='January 14, 2023 00:00:00 GMT+3:00' callback={() => alert('Hello')} /> */}
                 <h2> Our Story</h2>
                 <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
