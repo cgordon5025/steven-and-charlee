f683ee54e249e3f70c75334a4fb4fdc6fd358423
Charlee Gordon <cgordon5025@gmail.com>
2023-01-23 10:22:13 -0500

    more details on travel page

--- client/src/pages/Travel.js
+++ client/src/pages/Travel.js
@@ -7,6 +7,10 @@ const Travel = () => {
             <p> Hotels</p>
             <p> We have a hotelblock at this hotel, please sya you're witht eh Gordon&Tarr Party</p>
             <p> Transportation between Hotel and Venue</p>
+            <p> Directions from hotel to Venue</p>
+            <p> venue address</p>
+            <p> Fun things to do in the area?</p>
+            <p> include google maps?</p>
         </>
     )
 }
